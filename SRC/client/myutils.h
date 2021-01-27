5a11fc613d8aff4874250da7e5192a31e1753fda
StefanoBelli <sa.belli@hotmail.it>
2021-01-27 15:39:31 +0100

    moved some macros

--- SRC/client/myutils.h
+++ SRC/client/myutils.h
@@ -29,33 +29,6 @@ typedef struct {
 
 #define clear() (printf("\e[1;1H\e[2J"))
 
-#define checked_show_form(fields, fields_len) \
-	if(show_form(fields, fields_len) == FALSE) { \
-		puts("operazione annullata."); \
-		return FALSE; \
-	}
-
-#define checked_date_check(day, month, year) \
-	if(date_check(day, month, year) == FALSE) { \
-		puts("data inserita non valida!"); \
-		puts("operazione annullata."); \
-		return FALSE; \
-	}
-
-#define __basic_form_field(fields, idx, field_nm, expected_min, expected_max, output_sz, output_buf, output_ty) \
-	fields[idx].field_name = field_nm; \
-	fields[idx].expected_min_len = expected_min; \
-	fields[idx].expected_max_len = expected_max; \
-	fields[idx].output = output_buf; \
-	fields[idx].output_type = output_ty; \
-	fields[idx].output_size = output_sz;
-
-#define string_form_field(f, i, n, mi, ma, os, ob) \
-	__basic_form_field(f, i, n, mi, ma, os, ob, STRING)
-
-#define int_form_field(f, i, n, mi, ma, ob) \
-  __basic_form_field(f, i, n, mi, ma, 0, ob, INTEGER)
-
 mybool show_menu();
 mybool show_form(const form_field* fields, int fields_len);
 mybool date_check(int day, int month, int year);
--- SRC/client/op.h
+++ SRC/client/op.h
@@ -1,6 +1,8 @@
 #ifndef OP_H
 #define OP_H
 
+#include "myutils.h"
+
 #define checked_execute_stmt(stmt) \
 	if (!execute_stmt(stmt)) { \
 		puts("operazione annullata."); \
@@ -8,7 +10,33 @@
 		return FALSE; \
 	}
 
-#include "mybool.h"
+#define checked_show_form(fields, fields_len)   \
+	if (show_form(fields, fields_len) == FALSE) { \
+		puts("operazione annullata.");              \
+		return FALSE;                               \
+	}
+
+#define checked_date_check(day, month, year)   \
+	if (date_check(day, month, year) == FALSE) { \
+		puts("data inserita non valida!");         \
+		puts("operazione annullata.");             \
+		return FALSE;                              \
+	}
+
+#define __basic_form_field(fields, idx, field_nm, expected_min, expected_max, \
+                           output_sz, output_buf, output_ty)                  \
+  	fields[idx].field_name = field_nm;                                          \
+  	fields[idx].expected_min_len = expected_min;                                \
+  	fields[idx].expected_max_len = expected_max;                                \
+  	fields[idx].output = output_buf;                                            \
+  	fields[idx].output_type = output_ty;                                        \
+  	fields[idx].output_size = output_sz;
+
+#define string_form_field(f, i, n, mi, ma, os, ob) \
+  	__basic_form_field(f, i, n, mi, ma, os, ob, STRING)
+
+#define int_form_field(f, i, n, mi, ma, ob) \
+  	__basic_form_field(f, i, n, mi, ma, 0, ob, INTEGER)
 
 mybool manager_crea_nuovo_utente();
 mybool manager_ripristina_password_utente_esistente();
